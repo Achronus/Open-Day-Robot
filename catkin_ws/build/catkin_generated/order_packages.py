14b475e44d20625092027894f83d7a57351ac365
lewis <lewismcgillion@gmail.com>
2020-02-20 16:18:47 +0000

    updated workspace

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/lewis/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/lewis/catkin_ws/devel;/home/lewis/tiago_public_ws/devel;/opt/ros/melodic".split(';') if "/home/lewis/catkin_ws/devel;/home/lewis/tiago_public_ws/devel;/opt/ros/melodic" != "" else []
--- catkin_ws/build/open_day_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lewis/catkin_ws/src/open_day_robot
-# Build directory: /home/lewis/catkin_ws/build/open_day_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
