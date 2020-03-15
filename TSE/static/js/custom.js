9c8f581880ddd749950ae9f7314cd8475f44ea71
michaelcable01 <43931272+michaelcable01@users.noreply.github.com>
2020-03-15 16:14:17 +0000

    Add files via upload

---
+++ TSE/static/js/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // Prototype one buttons
+  $("#uni-btn").click(function() {
+    document.location.href = "questions-p1.html";
+  });
+  $("#det-btn").click(function() {
+    document.location.href = "questions-p1.html";
+  });
+  $("#acc-btn").click(function() {
+    document.location.href = "questions-p1.html";
+  });
+  $("#oth-btn").click(function() {
+    document.location.href = "questions-p1.html";
+  });
+
+  // Core buttons
+  $("#search-btn").click(function() {
+    // Search through database
+  });
+});
