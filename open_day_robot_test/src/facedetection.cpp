a4c7fac36adf02c3e26dc8dc84d8cb9ea6b604eb
lewis <lewismcgillion@gmail.com>
2020-03-20 15:04:22 +0000

    Version 1.1.0

--- open_day_robot_test/src/facedetection.cpp
+++ open_day_robot_test/src/facedetection.cpp
@@ -1,7 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "nav_msgs/Odometry.h"
-#include "pal_detection_msgs/Detections2d.h"
+#include "pal_detection_msgs/FaceDetections.h"
 #include "geometry_msgs/Twist.h"
 #include <sstream>
 #include <vector>
@@ -9,36 +9,33 @@
 //Creating robot class
 class Robot{
 public:
-    //Default robot constructor creating nodehandle, setting publisher and subscriber to velocity commands and person detection
+    //Default robot constructor creating nodehandle to handle the nodes, setting publisher and subscriber to velocity commands and person detection
     Robot(){
         ros::NodeHandle n;
         pub = n.advertise<geometry_msgs::Twist>("/mobile_base_controller/cmd_vel", 1000);
-        sub = n.subscribe("/person_detector/detections", 1000, &Robot::callBack, this);
+        sub = n.subscribe("/pal_face/faces", 1000, &Robot::callBack, this);
     }
 private:
     //Creating ros publisher and subscriber as private variables
     ros::Publisher pub;
     ros::Subscriber sub;
-    int imageHeight = 480; // height of person_detector/debug image in rows
-    int imageWidth = 640; // width of person_detector/debug image in columns
+    int imageHeight = 480; // height of /pal_face/debug image in rows
+    int imageWidth = 640; // width of /pal_face/debug image in columns
     int centreX = 0;
 
-    //Callback from person detection subscriber
-    void callBack(const pal_detection_msgs::Detections2d::ConstPtr &msg){
+    //Callback from subscriber
+    void callBack(const pal_detection_msgs::FaceDetections::ConstPtr &msg){
         geometry_msgs::Twist pubMsg;
 
-        if(msg->detections.data()!=NULL) {
-            const pal_detection_msgs::Detection2d *detection = msg->detections.data();
+        if(msg->faces.data() != NULL) {
+            const pal_detection_msgs::FaceDetection *detection = msg->faces.data();
 
-            ROS_INFO_STREAM(msg->detections.data());
-
-            //finding the centre coords of the person
+            //finding the centre coords of the person's face
             centreX = detection->x + (detection->width / 2);
 
-
-            if (centreX > (imageWidth * 0.45) && centreX < (imageWidth * 0.55)) {
+            if (centreX > (imageWidth * 0.40) && centreX < (imageWidth * 0.6)) {
                 //move forward
-                pubMsg.linear.x = 2.0;
+                pubMsg.linear.x = 0.5;
                 pubMsg.angular.z = 0.0;
             } else if (centreX < (imageWidth * 0.45)) {
                 //turn left
@@ -61,7 +58,7 @@ private:
 
 
 int main(int argc, char**argv){
-    //Initializing ros, creating a robot object and calling spin to loop 
+    //Initializing ros, creating a robot object and calling spin to loop
     ros::init(argc,argv,"tiago");
     Robot tiago;
     ros::spin();
